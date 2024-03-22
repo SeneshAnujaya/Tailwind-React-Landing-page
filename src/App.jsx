6b4cd99582fc4d575643f630a7669014a5f50e3e
unknown <senesh9637@gmail.com>
2024-03-23 00:28:38 +0530

    complete other sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { CustomerReviews, Hero, PopularProducts, SuperQuality, Services, Special
 import Nav from './components/Nav';
 
 const App = () => (
-  <main className="relative bg-slate-200">
+  <main className="relative">
     <Nav/>
     <section className="xl:padding-l wide:padding-r padding-b"><Hero/></section>
     <section className="padding"><PopularProducts/></section>
